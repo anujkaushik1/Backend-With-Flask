3d82423b78ead6e5a2ee1288784d5bd7a3331fbc
Anuj Kaushik <anujkaushik1512@gmail.com>
2021-11-29 17:35:49 +0530

    Implemented Render Template

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
-from flask import Flask   #from flask package we have imported Flask class
+from flask import Flask, render_template   #from flask package we have imported Flask class
 
 app = Flask(__name__)   #Flask class object and gives a name which is unique
 
-@app.route("/")
+@app.route("/firstpage")
 def hello_world():
-    return "Hello World"
+    return render_template("first_page.html")
 
-@app.route("/fancy")
+@app.route("/secondpage")
 def hello_world_fancy():
-    return """
-            <html>
-            <body>
-
-            <h1>Hello world</h1>
-            <p> My name is anuj </p>
-            
-
-            </body>
-            </html>
-            """
+    return render_template("second_page.html")
 
 
