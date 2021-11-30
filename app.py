f98984e782ec32e42bb4b272507a7defe8dc7854
Anuj Kaushik <anujkaushik1512@gmail.com>
2021-11-30 10:42:55 +0530

    Implemented Conditional Statements in Jinja2

--- app.py
+++ app.py
@@ -2,38 +2,13 @@ from flask import Flask, render_template   #from flask package we have imported 
 
 app = Flask(__name__)   #Flask class object and gives a name which is unique
 
-class GalileanMoons:
-    def __init__(self, first, second, third, fourth):
-        self.first = first
-        self.second = second
-        self.third = third
-        self.fourth = fourth
 
-@app.route("/data-structures/")
+@app.route("/conditional-basics/")
 def render_data_structures():
+    
+    company = "Apple"
 
-    movies = [
-        "Leon the professional",
-        "The usual Suspects",
-        "A beautiful mind"
-    ]
-
-    car = {
-        "brand" : "Tesla",
-        "model" : "Roadster",
-        "Year" : "2020"
-    }
-
-    moons = GalileanMoons("IO", "Europa", "Ganime", "Calisto")
-
-    kwargs = {
-        "movies" : movies,
-        "car" : car,
-        "moons" : moons
-    }
-
-
-    return render_template("data_structures.html", **kwargs)
+    return render_template("conditional_basics.html", company = company)
 
     
  
